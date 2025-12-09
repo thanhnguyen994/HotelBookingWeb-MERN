0c809cfb6c9e8fb7c25313206efdf0e7a6c8675d
thanhnguyen994 <nguyennt090904@gmail.com>
2025-12-09 21:02:59 +0700

    Trang chi tiết phòng

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import {FaFacebook, FaInstagram, FaYoutube} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex flex-col gap-12 px-16 py-16 bg-black text-white'>
+      {/*Top section */}
+      <div className='grid place-content-center gap-6 text-center'>
+        <h2 className='text-4xl font-bold'>Sign Up For Exclusive Offers</h2>
+        <div className='flex items-center justify-center max-w-xl max-auto w-full'>
+          <input type="email" placeholder='Enter your Email address' className='grow px-10 py-4 border-2 border-r-0 border-lime-500 rounded-l-full outline-none text-sm'/>
+          <button className='bg-lime-400 text-white px-8 py-4 rounded-r-full font-bold '>Join Now</button>
+        </div>
+      </div>
+      
+      {/*Bottom section */}
+      <div className='flex flex-col justify-between items-center text-center gap-6' >
+        <div >
+          <h2 className='text-2xl font-bold'>DELUXE HOTELS</h2>
+          <div className='flex justify-center gap-4 mt-3 text-lime-500'>
+            <FaFacebook className='text-3xl cursor-pointer'/>
+            <FaInstagram className='text-3xl cursor-pointer'/>
+            <FaYoutube className='text-3xl cursor-pointer'/>
+          </div>
+        </div>
+
+        <div>
+          <ul className='flex gap-6  justify-center text-base font-medium'>
+            <li className='cursor-pointer'>HOMES</li>
+            <li className='cursor-pointer'>BOOKINGS</li>
+            <li className='cursor-pointer'>ROOMS</li>
+            <li className='cursor-pointer'>CONTACT</li>
+          </ul>
+        </div>
+      </div>
+      <p className='text-center text-sm mt-4'>2025 DELUXE HOTELS. All rights reserved.</p>
+    </div>
   )
 }
 
--- frontend/src/pages/HotelDetails.jsx
+++ frontend/src/pages/HotelDetails.jsx
@@ -1,8 +1,80 @@
 import React from 'react'
+import {roomData} from '../assets/asset'
+import {useParams} from 'react-router-dom'
+import {FaConciergeBell, FaSwimmingPool, FaUtensils, FaWifi, FaTv} from 'react-icons/fa'
 
 const HotelDetail = () => {
+  const {id} = useParams()
+
+  const room = roomData.find((room) => {
+    return room.id === parseInt(id)
+  })
+
   return (
-    <div>HotelDetail</div>
+    <div className='mx-auto max-w-8xl p-6 grid grid-cols-1 md:grid-cols-3 gap-8'>
+      {/*Bên trái */}
+       <div className='md:col-span-2 space-y-6'>
+        <div>
+          <h1 className='text-3xl font-bold'>{room.name}</h1>
+          <p className='text-xl text-lime-500 mt-1'>${room.price}</p>
+        </div>
+        <img src={room.image} alt="" className='w-full rounded-lg shadow-md'/>
+        <div className='bg-gray-100 p-4 rounded-lg shadow-md'>
+          <h2 className='text-xl font-semibold mb-3'>Amenities</h2>
+          <div className='grid grid-cols-2 gap-4 text-gray-700'>
+            <div className='flex items-center gap-2'>
+              <FaWifi /> Wi - Fi
+            </div>
+            <div className='flex items-center gap-2'>
+              <FaTv /> Cable TV
+            </div>
+            <div className='flex items-center gap-2'>
+              <FaUtensils /> Restaurant
+            </div>
+            <div className='flex items-center gap-2'>
+              <FaSwimmingPool /> Pool
+            </div>
+            <div className='flex items-center gap-2'>
+              <FaConciergeBell /> Room Service
+            </div>
+          </div>
+
+          <div>
+            <h2 className='text-lg font-semibold mb-2'>Room Description </h2>
+            <p className='text-gray-600'>{room.description} </p>
+            <p className='text-gray-600'>{room.description} </p>
+            <p className='text-gray-600'>{room.description} </p>
+            <p className='text-gray-600'>{room.description} </p>
+          </div>
+        </div>
+       </div>
+      {/*Bên phải */}
+       <div className='bg-white p-6 mt-18 rounded-lg shadow-md'>
+        <h2 className='text-2xl font-bold mb-4'>Book Your Stay</h2>
+        <form  className='space-y-4'>
+          <input type="text" name="" placeholder='Name' className='w-full border border-b-gray-300 p-3 rounded-lg'/>
+          <input type="email" name="" placeholder='Email' className='w-full border border-b-gray-300 p-3 rounded-lg'/>
+          <input type="tel" name="" placeholder='Phone Number' className='w-full border border-b-gray-300 p-3 rounded-lg'/>
+          <div>
+            <label htmlFor="date" className='font-bold'>Check-In </label>
+            <input type="date" name="" id="" className='w-full border border-b-gray-300 p-3 rounded-lg' />
+          </div>
+          <div>
+            <label htmlFor="date" className='font-bold'>Check-Out</label>
+            <input type="date" name="" id="" className='w-full border border-b-gray-300 p-3 rounded-lg'/>
+          </div>
+          <div>
+            <label htmlFor="" className='font-bold'>Number of Guests</label>
+            <select name="" id="" className='w-full p-3 mb-3 border rounded-lg focus:ring focus:ring-blue-400'>
+              {[...Array(4).keys().map((i)=> (
+                <option key = {i + 1} value={i + 1}>{i + 1} Guests(s) </option>
+              ))]}
+            </select>
+          </div>
+          <button type="submit" className='w-full bg-lime-400 text-white p-3 rounded-lg hover:bg-lime-300 transition'>Book Now</button>
+        </form>
+       </div>
+    </div>
   )
 }
 
